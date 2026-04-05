47a1cf0e540eb8bc9d704cb4629161eef6124779
lowjichow <phucloidanghoconline@gmail.com>
2026-04-05 14:29:00 +0700

    feat(backend): log CRUD actions for core master data

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let categoryModel = require('../models/Category')
+let { logAction } = require('../utils/auditlogHandler')
 
 /* GET all categories */
 router.get('/', async function (req, res, next) {
@@ -32,6 +33,7 @@ router.post('/', async function (req, res) {
       parentCategory: req.body.parentCategory || null
     });
     await newCate.save();
+    logAction(req.user ? req.user._id : null, 'CREATE', 'category', newCate._id, newCate, req.ip)
     res.send(newCate);
   } catch (error) {
     res.status(400).send({ message: error.message });
@@ -42,8 +44,10 @@ router.post('/', async function (req, res) {
 router.put('/:id', async function (req, res) {
   try {
     let id = req.params.id;
+    let oldData = await categoryModel.findById(id);
     let result = await categoryModel.findByIdAndUpdate(id, req.body, { new: true });
     if (result) {
+      logAction(req.user ? req.user._id : null, 'UPDATE', 'category', id, { old: oldData, new: result }, req.ip)
       res.send(result);
     } else {
       res.status(404).send({ message: "ID NOT FOUND" });
@@ -59,6 +63,7 @@ router.delete('/:id', async function (req, res) {
     let id = req.params.id;
     let result = await categoryModel.findByIdAndDelete(id);
     if (result) {
+      logAction(req.user ? req.user._id : null, 'DELETE', 'category', id, result, req.ip)
       res.send({ message: "Deleted successfully", data: result });
     } else {
       res.status(404).send({ message: "ID NOT FOUND" });
